d2c1d6da7cd46b02989cc249662f369b7006e15a
JiwooJng <jjjw1225@gmail.com>
2025-05-19 17:44:52 +0900

    [refactor] Product Service 및 Test

--- src/test/java/kr/hhplus/be/test/product/ProductRedisServiceTest.java
+++ src/test/java/kr/hhplus/be/test/product/ProductRedisServiceTest.java
@@ -1,28 +1,25 @@
 package kr.hhplus.be.test.product;
 
 
-import kr.hhplus.be.ProductRedisService;
+import kr.hhplus.be.domain.product.ProductService;
+import kr.hhplus.be.domain.product.repository.ProductCacheRepository;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDate;
-import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @Testcontainers
 public class ProductRedisServiceTest {
 
     @Autowired
-    private ProductRedisService productRedisService;
+    private ProductService productService;
     @Autowired
-    private StringRedisTemplate redisTemplate;
+    private ProductCacheRepository productCacheRepository;
 
     private final String SALES_KEY = "product:sales";
 
@@ -31,13 +28,13 @@ public class ProductRedisServiceTest {
         // given
         long productId = 1L;
         long salesAmount = 10L;
-        Double score = redisTemplate.opsForZSet().score(SALES_KEY + LocalDate.now(), Long.toString(productId));
+        Double score = productCacheRepository.getScore(SALES_KEY + LocalDate.now(), Long.toString(productId));
 
         if (score == null) {
-            redisTemplate.opsForZSet().addIfAbsent(SALES_KEY + LocalDate.now(), Long.toString(productId), 20);
+            productCacheRepository.saveIfAbsent(SALES_KEY + LocalDate.now(), Long.toString(productId), 20);
         }
-        productRedisService.updateProductInfo(productId, salesAmount);
-        Double updateScore = redisTemplate.opsForZSet().score(SALES_KEY + LocalDate.now(), Long.toString(productId));
+        productService.updateProductInfo(productId, salesAmount);
+        Double updateScore = productCacheRepository.getScore(SALES_KEY + LocalDate.now(), Long.toString(productId));
 
         assertThat(updateScore).isEqualTo(30.0);
     }
